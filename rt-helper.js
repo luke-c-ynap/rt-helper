605a20cd144dc29f64f9e2271ca49ff097b7c83f
luke-c <luke.b.cano@gmail.com>
2024-11-25 22:03:15 +0000

    Update rt-helper.js

--- rt-helper.js
+++ rt-helper.js
@@ -25,7 +25,7 @@ footer.innerHTML = `
 <button id="runScript5" style="margin: 0 5px; padding: 5px 5px;" title="Open the Retouch pages for all VIDs in your Madame personal area">P.A VIDs</button>
 <button id="runScript6" style="margin: 0 5px; padding: 5px 5px;" title="Open Validator pages for all the VIDs on this Madame page">Open Vals</button>
 <button id="runScript7" style="margin: 0 5px; padding: 5px 5px;" title="Refresh the Madame search results">ReSearch</button>
-<button id="runScript8" style="margin: 0 5px; padding: 5px 5px;" title="Hide pop up notifications on Madame">Go Away</button>
+<button id="runScript8" style="margin: 0 5px; padding: 5px 5px;" title="Instantly kill pop up notifications on Madame">Go Away</button>
 <button id="runScript9" style="margin: 0 5px; padding: 5px 5px;" title="Nice">;)</button>
 `;
 
